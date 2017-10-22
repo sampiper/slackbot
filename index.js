95e5a28c7263d26a84f88360ceb94b6f23f84739
sampiper-tab <sam.piper@theappbusiness.com>
2017-10-22 11:42:25 +0100

    Echo token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  var token = req.body.token
+  res.send('Hello!' + token)
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
