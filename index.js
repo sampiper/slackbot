404bff88acb479b1fea242f0e618af00d59220cd
sampiper-tab <sam.piper@theappbusiness.com>
2017-10-22 11:57:01 +0100

    Revert "Change text"
    This reverts commit 8f1afc761b564e51c0b2c94cb4ee60e742c28e31.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const app = express()
 
 app.get('/', (req, res) => {
-  var token = req.body.token
-  res.send('Hello!' + token)
+  res.send('Hello!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
