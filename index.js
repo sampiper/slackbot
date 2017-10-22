eda450400ff995a304510b0bcaf0afaebae5e785
sampiper-tab <sam.piper@theappbusiness.com>
2017-10-22 11:58:23 +0100

    declare app

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const app = express()
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Hello!')
