49b7ae458e475776ffc8bcb6843cc2e73bc62f0e
sampiper-tab <sam.piper@theappbusiness.com>
2017-11-04 12:29:40 +0000

    na

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
 app.get('/devices', (req, res) => {
-  var token = req.token
-  res.send('Hello!' + token)
+  var token = req.body.token
+  res.json({
+    response_type: 'ephemeral',
+    text: 'hello'
+  })
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
