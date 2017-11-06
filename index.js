ecff8497839f6a1135aba7ed78809594309238ef
sampiper-tab <sam.piper@theappbusiness.com>
2017-11-06 09:29:56 +0000

    na

--- index.js
+++ index.js
@@ -63,19 +63,3 @@ function getDeviceList(res) {
   }
 
   app.listen(3000, () => console.log('Server running on port 3000'))
-
-
-/*
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-
-
-app.get('/devices', (req, res) => {
-  var token = req.body.token
-  res.json({
-    response_type: 'ephemeral',
-    text: 'hello'
-  })
-})
-app.listen(3000, () => console.log('Server running on port 3000'))*/
