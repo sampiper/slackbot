4afd65e13056187774e3e9a48398da96f5910551
sampiper-tab <sam.piper@theappbusiness.com>
2017-11-07 13:22:03 +0000

    updated help

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.route('/devices')
     else {
       res.json({
         response_type: 'ephemeral',
-        text: ':wave: *How to use the TAB Devicebot...*\n`/devices list` View a list of all devices.\n`/devices os [Android *.* | iOS *.*]` View all device running a specific OS version.'
+        text: '*How to use the TAB Devicebot...* :iphone:\n\n1) View a list of all devices:\n`/devices list`\nView all devices running a specific OS version.:\n`/devices os [Android *.* | iOS *.*]`'
       });
     }
   })
